7995c77390afaa738120db25dc70f7c1ce92b58b
Patrik Lindefors <patrik@lindefors.com>
2023-04-06 09:50:28 +0200

    Completed sectio 2.3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::io::{self, Read, Write};
+
+const CHUNK_SUZE: usize = 16 * 1024;
+
 fn main() {
-    println!("Hello, world!");
+    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+    let mut total_bytes = 0;
+    loop {
+        let mut buffer = [0; CHUNK_SUZE];
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        total_bytes += num_read;
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+    if !silent {
+        eprintln!("Bytes read: {total_bytes}");
+    }
 }
