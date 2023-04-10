8bc4af20da2f3ea16e560844bbccd41706ce3fc8
Patrik Lindefors <patrik@lindefors.com>
2023-04-10 22:23:00 +0200

    Cleaned the code a bit and noted two bugs!

--- src/read.rs
+++ src/read.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io::{self, BufReader, Read, Result};
 
 pub fn read(infile: &str) -> Result<Vec<u8>> {
+    // Todo: Don't create a BufReader every time!
     let mut reader: Box<dyn Read> = if infile.is_empty() {
         Box::new(BufReader::new(io::stdin()))
     } else {
--- src/write.rs
+++ src/write.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{self, BufWriter, ErrorKind, Result, Write};
 
 pub fn write(outfile: &str, buffer: &[u8]) -> Result<bool> {
+    // Todo: Don't create a BufWriter every time!
     let mut writer: Box<dyn Write> = if outfile.is_empty() {
         Box::new(BufWriter::new(io::stdout()))
     } else {
