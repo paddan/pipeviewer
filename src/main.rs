5d99163f340ee3ef59f60438d64f5dede8b3525d
Patrik Lindefors <patrik@lindefors.com>
2023-04-12 14:05:22 +0200

    Implemented threads and channels which fixed the bugs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
 use pipeviewer::{args::Args, read, stats, write};
 use std::io::Result;
+use std::sync::mpsc;
+use std::thread;
 
 fn main() -> Result<()> {
     let args = Args::get();
-    let mut total_bytes = 0;
-    let infile = args.infile.unwrap_or_default();
-    let outfile = args.outfile.unwrap_or_default();
-    let silent = args.silent;
+    let Args {
+        infile,
+        outfile,
+        silent,
+    } = args;
 
-    loop {
-        let buffer = match read::read(&infile) {
-            Ok(x) if x.is_empty() => break,
-            Ok(x) => x,
-            Err(e) => return Err(e),
-        };
+    let (stats_tx, stats_rx) = mpsc::channel();
+    let (write_tx, write_rx) = mpsc::channel();
 
-        stats::stats(silent, buffer.len(), &mut total_bytes, false);
-        if !write::write(&outfile, &buffer)? {
-            break;
-        }
-    }
-    stats::stats(silent, 0, &mut total_bytes, true);
+    let read_handle = thread::spawn(move || read::read_loop(&infile.unwrap_or_default(), stats_tx));
+    let stats_handle = thread::spawn(move || stats::stats_loop(silent, stats_rx, write_tx));
+    let write_handle =
+        thread::spawn(move || write::write_loop(&outfile.unwrap_or_default(), write_rx));
+
+    read_handle.join().unwrap()?;
+    stats_handle.join().unwrap()?;
+    write_handle.join().unwrap()?;
 
     Ok(())
 }
--- src/read.rs
+++ src/read.rs
@@ -1,17 +1,29 @@
-use crate::CHUNK_SUZE;
 use std::fs::File;
 use std::io::{self, BufReader, Read, Result};
+use std::sync::mpsc::Sender;
 
-pub fn read(infile: &str) -> Result<Vec<u8>> {
-    // Todo: Don't create a BufReader every time!
+use crate::CHUNK_SUZE;
+
+pub fn read_loop(infile: &str, stats_tx: Sender<Vec<u8>>) -> Result<()> {
     let mut reader: Box<dyn Read> = if infile.is_empty() {
         Box::new(BufReader::new(io::stdin()))
     } else {
-        Box::new(BufReader::new(File::open(infile)?)) // TODO: This will only read the first 16k every time!
+        Box::new(BufReader::new(File::open(infile)?))
     };
 
     let mut buffer = [0; CHUNK_SUZE];
-    let num_read = reader.read(&mut buffer)?;
+    loop {
+        let num_read = match reader.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(e) => return Err(e),
+        };
 
-    Ok(Vec::from(&buffer[..num_read]))
+        if stats_tx.send(Vec::from(&buffer[..num_read])).is_err() {
+            break;
+        }
+    }
+
+    let _ = stats_tx.send(Vec::new());
+    Ok(())
 }
