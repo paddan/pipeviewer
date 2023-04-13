0dfd3ac5cc8e4e2ca0fe366cb6061a835068fec2
Patrik Lindefors <patrik@lindefors.com>
2023-04-13 14:08:46 +0200

    Changed from mpsc channels to crossbeam channels

--- src/read.rs
+++ src/read.rs
@@ -1,10 +1,10 @@
 use std::fs::File;
 use std::io::{self, BufReader, Read, Result};
-use std::sync::mpsc::Sender;
+use crossbeam::channel::Sender;
 
 use crate::CHUNK_SUZE;
 
-pub fn read_loop(infile: &str, stats_tx: Sender<Vec<u8>>) -> Result<()> {
+pub fn read_loop(infile: &str, stats_tx: Sender<usize>, write_tx: Sender<Vec<u8>>) -> Result<()> {
     let mut reader: Box<dyn Read> = if infile.is_empty() {
         Box::new(BufReader::new(io::stdin()))
     } else {
@@ -19,11 +19,14 @@ pub fn read_loop(infile: &str, stats_tx: Sender<Vec<u8>>) -> Result<()> {
             Err(e) => return Err(e),
         };
 
-        if stats_tx.send(Vec::from(&buffer[..num_read])).is_err() {
+        let _ = stats_tx.send(num_read);
+
+        if write_tx.send(Vec::from(&buffer[..num_read])).is_err() {
             break;
         }
     }
 
-    let _ = stats_tx.send(Vec::new());
+    let _ = stats_tx.send(0);
+    let _ = write_tx.send(Vec::new());
     Ok(())
 }
